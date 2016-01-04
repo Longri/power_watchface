c8a3ca0d3062221acc4656aee85a18140730a4d4
www.longri.de@googlemail.com <www.longri.de@googlemail.com>
2016-01-04 16:34:33 +0100

    + remove unused methods

--- Wearable/src/main/java/de/longri/watchface/Utils.java
+++ Wearable/src/main/java/de/longri/watchface/Utils.java
@@ -1,7 +1,10 @@
 package de.longri.watchface;
 
 
+import android.graphics.Canvas;
 import android.graphics.ColorMatrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
 
 
 /**
@@ -15,19 +18,20 @@ public final class Utils {
 
 
     // TODO: 04.01.2016 REMOVE unused
+    private static Rect bounds = new Rect();
 
+    public static void drawString(Canvas canvas, final Paint paint, final String str, int x, int y) {
+        final String[] lines = str.split("\n");
 
+        int yOff = 0;
+        for (int i = 0; i < lines.length; ++i) {
+            canvas.drawText(lines[i], x, y + yOff, paint);
+            paint.getTextBounds(lines[i], 0, lines[i].length(), bounds);
+            yOff += bounds.height();
+        }
+    }
 
-
-    static public final float nanoToSec = 1 / 1000000000f;
-
-    // ---
-    static public final float FLOAT_ROUNDING_ERROR = 0.000001f; // 32 bits
     static public final float PI = 3.1415927f;
-    static public final float PI2 = PI * 2;
-
-    static public final float E = 2.7182818f;
-
     static private final int SIN_BITS = 14; // 16KB. Adjust for accuracy.
     static private final int SIN_MASK = ~(-1 << SIN_BITS);
     static private final int SIN_COUNT = SIN_MASK + 1;
@@ -41,12 +45,11 @@ public final class Utils {
      * multiply by this to convert from radians to degrees
      */
     static public final float radiansToDegrees = 180f / PI;
-    static public final float radDeg = radiansToDegrees;
     /**
      * multiply by this to convert from degrees to radians
      */
     static public final float degreesToRadians = PI / 180;
-    static public final float degRad = degreesToRadians;
+
 
     static private class Sin {
         static final float[] table = new float[SIN_COUNT];
@@ -71,238 +74,6 @@ public final class Utils {
      */
     static public float cos(float radians) {
         return Sin.table[(int) ((radians + PI / 2) * radToIndex) & SIN_MASK];
-    }
-
-    /**
-     * Returns the sine in radians from a lookup table.
-     */
-    static public float sinDeg(float degrees) {
-        return Sin.table[(int) (degrees * degToIndex) & SIN_MASK];
-    }
-
-    /**
-     * Returns the cosine in radians from a lookup table.
-     */
-    static public float cosDeg(float degrees) {
-        return Sin.table[(int) ((degrees + 90) * degToIndex) & SIN_MASK];
-    }
-
-    // ---
-
-    static private final int ATAN2_BITS = 7; // Adjust for accuracy.
-    static private final int ATAN2_BITS2 = ATAN2_BITS << 1;
-    static private final int ATAN2_MASK = ~(-1 << ATAN2_BITS2);
-    static private final int ATAN2_COUNT = ATAN2_MASK + 1;
-    static final int ATAN2_DIM = (int) Math.sqrt(ATAN2_COUNT);
-    static private final float INV_ATAN2_DIM_MINUS_1 = 1.0f / (ATAN2_DIM - 1);
-
-    static private class Atan2 {
-        static final float[] table = new float[ATAN2_COUNT];
-
-        static {
-            for (int i = 0; i < ATAN2_DIM; i++) {
-                for (int j = 0; j < ATAN2_DIM; j++) {
-                    float x0 = (float) i / ATAN2_DIM;
-                    float y0 = (float) j / ATAN2_DIM;
-                    table[j * ATAN2_DIM + i] = (float) Math.atan2(y0, x0);
-                }
-            }
-        }
-    }
-
-    /**
-     * Returns atan2 in radians from a lookup table.
-     */
-    static public float atan2(float y, float x) {
-        float add, mul;
-        if (x < 0) {
-            if (y < 0) {
-                y = -y;
-                mul = 1;
-            } else
-                mul = -1;
-            x = -x;
-            add = -PI;
-        } else {
-            if (y < 0) {
-                y = -y;
-                mul = -1;
-            } else
-                mul = 1;
-            add = 0;
-        }
-        float invDiv = 1 / ((x < y ? y : x) * INV_ATAN2_DIM_MINUS_1);
-
-        if (invDiv == Float.POSITIVE_INFINITY) return ((float) Math.atan2(y, x) + add) * mul;
-
-        int xi = (int) (x * invDiv);
-        int yi = (int) (y * invDiv);
-        return (Atan2.table[yi * ATAN2_DIM + xi] + add) * mul;
-    }
-
-    /**
-     * Returns the next mPowerWatchDrawable of two. Returns the specified value if the value is already a mPowerWatchDrawable of two.
-     */
-    static public int nextPowerOfTwo(int value) {
-        if (value == 0) return 1;
-        value--;
-        value |= value >> 1;
-        value |= value >> 2;
-        value |= value >> 4;
-        value |= value >> 8;
-        value |= value >> 16;
-        return value + 1;
-    }
-
-    static public boolean isPowerOfTwo(int value) {
-        return value != 0 && (value & value - 1) == 0;
-    }
-
-    // ---
-
-    static public short clamp(short value, short min, short max) {
-        if (value < min) return min;
-        if (value > max) return max;
-        return value;
-    }
-
-    static public int clamp(int value, int min, int max) {
-        if (value < min) return min;
-        if (value > max) return max;
-        return value;
-    }
-
-    static public long clamp(long value, long min, long max) {
-        if (value < min) return min;
-        if (value > max) return max;
-        return value;
-    }
-
-    static public float clamp(float value, float min, float max) {
-        if (value < min) return min;
-        if (value > max) return max;
-        return value;
-    }
-
-    static public double clamp(double value, double min, double max) {
-        if (value < min) return min;
-        if (value > max) return max;
-        return value;
-    }
-
-    // ---
-
-    /**
-     * Linearly interpolates between fromValue to toValue on progress position.
-     */
-    static public float lerp(float fromValue, float toValue, float progress) {
-        return fromValue + (toValue - fromValue) * progress;
-    }
-
-    // ---
-
-    static private final int BIG_ENOUGH_INT = 16 * 1024;
-    static private final double BIG_ENOUGH_FLOOR = BIG_ENOUGH_INT;
-    static private final double CEIL = 0.9999999;
-    static private final double BIG_ENOUGH_CEIL = 16384.999999999996;
-    static private final double BIG_ENOUGH_ROUND = BIG_ENOUGH_INT + 0.5f;
-
-    /**
-     * Returns the largest integer less than or equal to the specified float. This method will only properly floor floats from
-     * -(2^14) to (Float.MAX_VALUE - 2^14).
-     */
-    static public int floor(float value) {
-        return (int) (value + BIG_ENOUGH_FLOOR) - BIG_ENOUGH_INT;
-    }
-
-    /**
-     * Returns the largest integer less than or equal to the specified float. This method will only properly floor floats that are
-     * positive. Note this method simply casts the float to int.
-     */
-    static public int floorPositive(float value) {
-        return (int) value;
-    }
-
-    /**
-     * Returns the smallest integer greater than or equal to the specified float. This method will only properly ceil floats from
-     * -(2^14) to (Float.MAX_VALUE - 2^14).
-     */
-    static public int ceil(float value) {
-        return (int) (value + BIG_ENOUGH_CEIL) - BIG_ENOUGH_INT;
-    }
-
-    /**
-     * Returns the smallest integer greater than or equal to the specified float. This method will only properly ceil floats that
-     * are positive.
-     */
-    static public int ceilPositive(float value) {
-        return (int) (value + CEIL);
-    }
-
-    /**
-     * Returns the closest integer to the specified float. This method will only properly round floats from -(2^14) to
-     * (Float.MAX_VALUE - 2^14).
-     */
-    static public int round(float value) {
-        return (int) (value + BIG_ENOUGH_ROUND) - BIG_ENOUGH_INT;
-    }
-
-    /**
-     * Returns the closest integer to the specified float. This method will only properly round floats that are positive.
-     */
-    static public int roundPositive(float value) {
-        return (int) (value + 0.5f);
-    }
-
-    /**
-     * Returns true if the value is zero (using the default tolerance as upper bound)
-     */
-    static public boolean isZero(float value) {
-        return Math.abs(value) <= FLOAT_ROUNDING_ERROR;
-    }
-
-    /**
-     * Returns true if the value is zero.
-     *
-     * @param tolerance represent an upper bound below which the value is considered zero.
-     */
-    static public boolean isZero(float value, float tolerance) {
-        return Math.abs(value) <= tolerance;
-    }
-
-    /**
-     * Returns true if a is nearly equal to b. The function uses the default floating error tolerance.
-     *
-     * @param a the first value.
-     * @param b the second value.
-     */
-    static public boolean isEqual(float a, float b) {
-        return Math.abs(a - b) <= FLOAT_ROUNDING_ERROR;
-    }
-
-    /**
-     * Returns true if a is nearly equal to b.
-     *
-     * @param a         the first value.
-     * @param b         the second value.
-     * @param tolerance represent an upper bound below which the two values are considered equal.
-     */
-    static public boolean isEqual(float a, float b, float tolerance) {
-        return Math.abs(a - b) <= tolerance;
-    }
-
-    /**
-     * @return the logarithm of value with base a
-     */
-    static public float log(float a, float value) {
-        return (float) (Math.log(value) / Math.log(a));
-    }
-
-    /**
-     * @return the logarithm of value with base 2
-     */
-    static public float log2(float value) {
-        return log(2, value);
     }
 
 
