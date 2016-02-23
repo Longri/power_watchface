c8c087fe86b4ae4d938075adf446a81280d5beb7
www.longri.de@googlemail.com <www.longri.de@googlemail.com>
2016-02-23 17:50:41 +0100

    + add day of week drawing on tinyDateDrawable

--- Wearable/src/main/java/de/longri/watchface/TinyDateDrawable.java
+++ Wearable/src/main/java/de/longri/watchface/TinyDateDrawable.java
@@ -24,7 +24,6 @@ import android.text.format.DateFormat;
 
 import java.util.Date;
 
-
 /**
  * Created by Longri on 18.11.2015.
  */
@@ -39,7 +38,6 @@ public class TinyDateDrawable extends TinyDrawable {
 
     private int phonePowerPercent = 0;
 
-
     private int mLastDay = -1;
 
     public TinyDateDrawable(Context context) {
@@ -51,7 +49,8 @@ public class TinyDateDrawable extends TinyDrawable {
     }
 
     public boolean mustRedraw() {
-        if (mLastDay != RES.mTime.monthDay) return true;
+        if (mLastDay != RES.mTime.monthDay)
+            return true;
         return super.mustRedraw();
     }
 
@@ -59,7 +58,6 @@ public class TinyDateDrawable extends TinyDrawable {
         phonePowerPercent = value;
         this.setMustRedraw();
     }
-
 
     Bitmap bufferBitmap;
 
@@ -84,7 +82,6 @@ public class TinyDateDrawable extends TinyDrawable {
 
             final Canvas bufferCanvas = new Canvas(bufferBitmap);
 
-
             float scale = Theme.scaleFactor;
 
             final Paint datePaint = new Paint();
@@ -97,7 +94,6 @@ public class TinyDateDrawable extends TinyDrawable {
             dateNamePaint.setTextSize(28 * scale);
             dateNamePaint.setAntiAlias(true);
 
-
             final String dateString = Integer.toString(RES.mTime.monthDay);
             final String dateNameString = (String) DateFormat.format("MMM", new Date());
 
@@ -105,15 +101,81 @@ public class TinyDateDrawable extends TinyDrawable {
             final float xPositionDateName = width / 2 - dateNamePaint.measureText(dateNameString) / 2;
 
             drawTinyBackground(bufferCanvas, EMPTY_MATRIX);
-            bufferCanvas.drawText(dateString, xPositionDate, 50 * scale, datePaint);
-            bufferCanvas.drawText(dateNameString, xPositionDateName, 80 * scale, dateNamePaint);
+            bufferCanvas.drawText(dateString, xPositionDate, 55 * scale, datePaint);
+            bufferCanvas.drawText(dateNameString, xPositionDateName, 85 * scale, dateNamePaint);
+
+            // draw week day
+            final Paint dayPaint = new Paint();
+            dayPaint.setColor(RES.getTextColor());
+            dayPaint.setTextSize(10 * scale);
+            dayPaint.setAntiAlias(true);
+
+            final Paint actDayPaint = new Paint();
+            actDayPaint.setARGB(255, 255, 0, 0);
+            actDayPaint.setTextSize(12 * scale);
+            actDayPaint.setAntiAlias(true);
+
+            String[] days = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+            // act day index, shift to 'Mon' is first
+            int actDayIndex = RES.mTime.weekDay - 1;
+            if (actDayIndex < 0)
+                actDayIndex = 6;
+
+            // measure additional width!
+            // "MON" equals margin
+            float length = 0;
+            float margin = -1;
+            float[] deg = new float[7];
+
+            int idx = 0;
+            for (String day : days) {
+                if (margin == -1) {
+                    margin = dayPaint.measureText(day) / 4;
+                }
+
+                float dayLength;
+                if (idx == actDayIndex) {
+                    dayLength = actDayPaint.measureText(day);
+                } else {
+                    dayLength = dayPaint.measureText(day);
+                }
+
+                deg[idx] = (float) ((dayLength * 180) / (Math.PI * ((width - 28) / 2)));
+
+                length += dayLength;
+                if (idx++ < 6) {
+                    length += margin;
+                }
+            }
+
+            float marginDeg = (float) ((margin * 180) / (Math.PI * ((width - 30) / 2)));
+            float lengthDeg = (float) ((length * 180) / (Math.PI * ((width - 30) / 2)));
+
+            Path path = new Path();
+            path.addArc(new RectF(15, 15, width - 15, width - 15), 0, 90);
+            Matrix rotate = new Matrix();
+
+            rotate.postRotate(-(lengthDeg / 2) - 90, width / 2, width / 2);
+
+            idx = 0;
+            for (String day : days) {
+
+                bufferCanvas.setMatrix(rotate);
+                bufferCanvas.drawTextOnPath(day, path, 0, 0, (idx == actDayIndex) ? actDayPaint : dayPaint);
+
+                rotate.postRotate(deg[idx++] + marginDeg, width / 2, width / 2);
+                //		if (idx == 3)
+                //		    break;
+            }
 
             //draw Power
+            bufferCanvas.setMatrix(null);
             mPowerWatchDrawable.setPowerPercent(getBatteryPercent());
-            mPowerWatchDrawable.draw(bufferCanvas, width, height);
+            mPowerWatchDrawable.draw(bufferCanvas, width + 2, height + 2);
 
             mPowerPhoneDrawable.setPowerPercent(phonePowerPercent);
-            mPowerPhoneDrawable.draw(bufferCanvas, width, height);
+            mPowerPhoneDrawable.draw(bufferCanvas, width + 2, height + 2);
 
             fromBuffer = false;
         }
@@ -127,7 +189,6 @@ public class TinyDateDrawable extends TinyDrawable {
         Intent batteryStatus = mContext.registerReceiver(null, iFilter);
         return batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
     }
-
 
     @Override
     public boolean drawFull(Canvas canvas, Matrix matrix) {
